155092062f431f57f0c34b52fcbba84fb9aaaf5f
naimah123 <naimahabdulsamad@gmail.com>
2016-05-06 06:53:27 +0100

    java

---
+++ socketProgrammingJava.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+
+public class GetMyIp{
+	public static void main(String[] args)
+		URL url = null;
+		BufferedReader in = null;
+		String ipAddress = "";
+		try{
+		url = new URL("http://bot.whatismyipaddress.com");
+		in = new BufferedReader(new InputStreamReader(url.openStream()));
+		ipAddress = in.readLine().trim();
+		if(!(ipAddress.length()>0)){
+		try{
+			InetAddress ip = InetAddress.getLocalHost();
+			System.out.println((ip.getHostAddress()).trim());
+			ipAddress = (ip.getHostAddress()).trim();
+		}
+		catch(Exception exp)
+		{
+			ipAddress = "ERROR";
+		}
+		}
+		System.out.println("IP Address: " + ipAddress);
+		}
+}
